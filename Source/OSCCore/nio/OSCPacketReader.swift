e0ff006668dc2fd53008e52f20deb7dc30560f49
mron <ronmoreland@att.net>
2022-04-06 07:17:06 -0700

    Changed cxt to context

--- Source/OSCCore/nio/OSCPacketReader.swift
+++ Source/OSCCore/nio/OSCPacketReader.swift
@@ -14,20 +14,32 @@ public final class OSCPacketReader: ChannelInboundHandler {
 
     public init() {}
 
-    public func channelRead(ctx: ChannelHandlerContext, data: NIOAny) {
+    public func channelActive(context: ChannelHandlerContext) {
+       // print("Client connected to \(context.remoteAddress!)")
+//        if let ra = context.remoteAddress {
+//            print("Client connected to \(ra)")
+//        } else {
+//            print("Client connected to NIL!!!")
+//        }
+    }
+    
+    public func channelRead(context: ChannelHandlerContext, data: NIOAny) {
+//        print( "in channel read\n")
         let addressedEnvelope = self.unwrapInboundIn(data)
 
         if let rawBytes: [Byte] = addressedEnvelope.data.getBytes(at: 0, length: addressedEnvelope.data.readableBytes),
             let packet = decodeOSCPacket(from: rawBytes) {
-            ctx.fireChannelRead(self.wrapInboundOut(packet))
+            context.fireChannelRead(self.wrapInboundOut(packet))
         }
     }
 
-    public func channelReadComplete(ctx: ChannelHandlerContext) {
-        ctx.flush()
+    public func channelReadComplete(context: ChannelHandlerContext) {
+//        print( "in channel complete\n")
+        context.flush()
     }
 
-    public func errorCaught(ctx: ChannelHandlerContext, error: Error) {
-        ctx.close(promise: nil)
+    public func errorCaught(context: ChannelHandlerContext, error: Error) {
+//        print( "in channel error:   \(error)\n")
+        context.close(promise: nil)
     }
 }
